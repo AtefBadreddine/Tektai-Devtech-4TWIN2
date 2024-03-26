37f9682f68c94c401e420d431ca0073e2b7bde56
alaedineibrahim <alaedineibrahim@gmail.com>
2024-03-27 00:49:28 +0100

    fix error

--- tektai-backend/src/submission/submission.module.ts
+++ tektai-backend/src/submission/submission.module.ts
@@ -4,16 +4,25 @@ import { MulterModule } from '@nestjs/platform-express';
 import { SubmissionController } from './submission.controller';
 import { SubmissionService } from './submission.service';
 import { Submission, SubmissionSchema } from 'src/schemas/submission.schema';
+import { TeamsService } from 'src/teams/teams.service';
+import { Team, TeamSchema } from 'src/schemas/team.schema';
+// Import de ChallengesService
+import { Challenges, ChallengesSchema } from 'src/schemas/challenges.schema'; // Import de ChallengesModel
+import { ChallengesService } from 'src/challenge/challenges.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Submission.name, schema: SubmissionSchema }]),
+   MongooseModule.forFeature([
+      { name: Submission.name, schema: SubmissionSchema },
+      { name: Team.name, schema: TeamSchema },
+      { name: Challenges.name, schema: ChallengesSchema }, // Ajouter ChallengesModel au MongooseModule
+    ]),
     MulterModule.register({
-      dest: './uploads', // spécifiez le répertoire de destination pour enregistrer les fichiers téléchargés
+      dest: './uploads',
     }),
   ],
   controllers: [SubmissionController],
-  providers: [SubmissionService],
+  providers: [SubmissionService, TeamsService, ChallengesService],
   exports: [SubmissionService],
 })
 export class SubmissionModule {}
