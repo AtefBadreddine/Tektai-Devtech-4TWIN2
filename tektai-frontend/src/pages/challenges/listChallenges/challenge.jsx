6d07c12cfb3388ceb78c66c4f1075b8f58960e60
balkis hmidi <balkis.hmidi@esprit.tn>
2024-04-13 00:14:27 +0100

    challenge page

--- tektai-frontend/src/pages/challenges/listChallenges/challenge.jsx
+++ tektai-frontend/src/pages/challenges/listChallenges/challenge.jsx
@@ -42,23 +42,20 @@ const Challenges = ({ status }) => {
   const Challenge = ({ challenge, index }) => {
     const [companyName, setCompanyName] = useState('');
     const [loadingCompany, setLoadingCompany] = useState(true);
- 
-    function generateColor(index) {
-      const colors = [
-        'linear-gradient(#e66465, #9198e5)', // Tomato to Blueberry
-        'linear-gradient(#66b3ff, #99ff99)', // Blue Cheese to Lettuce
-        'linear-gradient(#ffcc99, #ffd700)', // Cheese to Pineapple
-        'linear-gradient(#ffa07a, #ff6347)', // Shrimp to Pepperoni
-        'linear-gradient(#adff2f, #9370db)', // Broccoli to Grape
-        'linear-gradient(#e66465, #66b3ff)', // Tomato to Blue Cheese
-        'linear-gradient(#9198e5, #ffd700)', // Blueberry to Pineapple
-        'linear-gradient(#ff6347, #9370db)', // Pepperoni to Grape
-        'linear-gradient(#adff2f, #66b3ff)', // Broccoli to Blue Cheese
-        'linear-gradient(#e66465, #ffa07a)', // Tomato to Shrimp
-        // Add more linear gradients as needed
+    function generateImg(index) {
+      const imgs = [
+        'https://images.pexels.com/photos/5011647/pexels-photo-5011647.jpeg?cs=srgb&dl=pexels-rostislav-uzunov-5011647.jpg&fm=jpg',
+        'https://img.freepik.com/premium-photo/3d-rendering-digital-art-illustration-variety-shapes_861655-30.jpg',
+        'https://png.pngtree.com/thumb_back/fh260/background/20230519/pngtree-3d-cube-background-abstract-texture-wallpaper-image_2599710.jpg',
+        'https://miro.medium.com/v2/resize:fit:1400/0*JG9ZJlqifvrgxS52',
+        'https://img.freepik.com/premium-photo/3d-rendering-futuristic-abstract-structure-with-intricate-complex-geometries-as-modern-trendy_896648-1227.jpg',
+        'https://images.unsplash.com/photo-1622547748225-3fc4abd2cca0?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbHBhcGVyJTIwM2R8ZW58MHx8MHx8fDA%3D',
+
+       
       ];
-      return colors[index % colors.length];
+      return imgs[index % imgs.length];
     }
+    
     
     
     
@@ -85,7 +82,18 @@ const Challenges = ({ status }) => {
 
   
       <div className="card" >
-      <div className="img" style={{ background: generateColor(index) }}>
+      <div className="img" style={{ backgroundImage: `url(${generateImg(index)})` }}>
+
+      <div className="stats pl-4">
+  <div className="viewer">
+    <span><img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" height="25px" width="25px" alt="Viewer 1" /></span>
+    <span><img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" height="25px" width="25px"   alt="Viewer 2" /></span>
+    <span><img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" height="25px" width="25px"   alt="Viewer 3" /></span>
+    <span className='smaller'>+20</span>
+  </div>
+</div>
+
+
     <div className="save">
     <div >
 <input type="checkbox" class="checkbox" id="checkbox" />
@@ -138,16 +146,18 @@ const Challenges = ({ status }) => {
 </div>
     </div>
   </div>
-  <Link to={`/challenges/${challenge._id}`} >
 
   <div className="text">
     <p className="h3">{truncateText(challenge.title, 15)}</p>
     <div className="flex items-center">
-  <h4 className="p">Company: </h4>
-  <h4 className="company">{loadingCompany ? 'Loading...' : companyName}</h4>
+    <h4 className="company pr-1">
+     by 
+    </h4>
+    <h4 className="company"> {loadingCompany ? 'Loading...' : companyName}</h4>
 </div>
 
-    <div className="icon-box">
+<div className="aline">
+  <div className="icon-box">
 
 <svg height="10px" width="10px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
 	 viewBox="0 0 451.827 451.827" xml:space="preserve">
@@ -159,11 +169,58 @@ const Challenges = ({ status }) => {
 </g>
 </svg>
       <p className="span">{challenge.status}</p>
+      
     </div>
-  </div>
+    <div class="btn-conteiner">
+    <Link to={`/challenges/${challenge._id}`} >
+  <a href="#" class="btn-content">
+    <span class="icon-arrow">
+      <svg
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        viewBox="0 0 66 43"
+        height="30px"
+        width="30px"
+      >
+        <g
+          fill-rule="evenodd"
+          fill="none"
+          stroke-width="1"
+          stroke="none"
+          id="arrow"
+        >
+          <path
+            fill="#9ee5fa"
+            d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
+            id="arrow-icon-one"
+          ></path>
+          <path
+            fill="#9ee5fa"
+            d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
+            id="arrow-icon-two"
+          ></path>
+          <path
+            fill="#9ee5fa"
+            d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
+            id="arrow-icon-three"
+          ></path>
+        </g>
+      </svg>
+    </span>
+  </a>
   </Link>
 </div>
+</div>
 
+  </div>
+
+
+
+
+</div>
+
+ 
 
 
 
