2a27ed08d82bd4cabce8b722414458fe56346222
balkis hmidi <balkis.hmidi@esprit.tn>
2024-04-13 23:36:52 +0100

    update2profilecomp

--- tektai-frontend/src/components/profileCompany.jsx
+++ tektai-frontend/src/components/profileCompany.jsx
@@ -301,6 +301,24 @@ function ProfileCompany() {
   </div>
 </div>
 
+<div className="card" >
+ 
+    <h1> Contact us</h1>
+<form class="cf">
+  <div class="half left cf">
+    <input type="text" id="input-name" placeholder="Name"/>
+    <input type="email" id="input-email" placeholder="Email address"/>
+    <input type="text" id="input-subject" placeholder="Subject"/>
+  </div>
+  <div class="half right cf">
+    <textarea name="message" type="text" id="input-message" placeholder="Message"></textarea>
+  </div>
+  <input type="submit" value="Submit" id="input-submit"/>
+</form>    
+
+
+</div>
+
 
    <div class="trends">
     <a href="#">
