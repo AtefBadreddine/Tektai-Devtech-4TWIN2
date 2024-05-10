b9edd58dba96a313d80f2e0fe4fba14e780cd8bc
AtefBadreddine <atef.badreddine@esprit.tn>
2024-05-10 09:48:56 +0100

    Update main.ts

--- tektai-backend/src/main.ts
+++ tektai-backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       AppModule,
   );
   app.enableCors({
-    origin: 'http://localhost:5173', // Specify the allowed origin
+    origin: '*', // Specify the allowed origin
     methods: ['GET', 'POST','DELETE','PUT'], // Specify the allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers
     credentials: true, // Specify if credentials (e.g., cookies) should be allowed to be sent
